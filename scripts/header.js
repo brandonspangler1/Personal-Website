5a420941b2c373e748465fe285a9bbd4a7ccdd5a
Brandon  Spangler <brandonspangler@Brandons-MBP.lan>
2022-09-30 18:49:51 -0400

    updated styles

--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,32 @@
+// Header JS
+
+const mainSection = document.getElementById('home');
+const projectSection = document.getElementById('projects');
+const aboutSection = document.getElementById('about');
+const footerSection = document.getElementById('footer');
+
 var flag = true;
 function menuTransform(x) {
     x.classList.toggle("change");
+    const headerHeight = document.getElementById("header").offsetHeight;
+    // console.log(headerHeight);
     document.getElementById("hamburgerMenuWrapper").classList.toggle("change");
     if (flag)
+    {
         hamburgerMenu.style.transform = "translate(0," + (-headerHeight/2) + "px)";
-    else 
+        mainSection.style.filter = 'blur(20px)';
+        projectSection.style.filter = 'blur(20px)';
+        aboutSection.style.filter = 'blur(20px)';
+        footerSection.style.filter = 'blur(20px)';
+    }
+    else
+    { 
         hamburgerMenu.style.transform = "translate(250%," + (-headerHeight/2) + "px)";
+        mainSection.style.filter = 'blur(0)';
+        projectSection.style.filter = 'blur(0)';
+        aboutSection.style.filter = 'blur(0)';
+        footerSection.style.filter = 'blur(0)';
+    }
 
     flag = !flag;
 }
@@ -18,12 +39,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const firstHamburgerButton = document.getElementById("firstHamburgerButton");
 
     hamburgerMenu.style.transform = "translate(250%," + (-headerHeight/2) + "px)";
-    firstHamburgerButton.style.marginTop = headerHeight + "px";
+    firstHamburgerButton.style.marginTop = (headerHeight + 16) + "px";
 });
 
 window.addEventListener('resize', function(){
     headerHeight = document.getElementById("header").offsetHeight;
-    firstHamburgerButton.style.marginTop = headerHeight + "px";
+    firstHamburgerButton.style.marginTop = (headerHeight + 16) + "px";
     // console.log(headerHeight);
 });
 
@@ -70,13 +91,13 @@ headerNameTween = gsap.to("#headerName", {
             
             
             headerNameWidth = headerName.offsetWidth;
-            console.log(headerNameWidth);  
+            // console.log(headerNameWidth);  
             
             
             headerPicWidth = headerPic.clientWidth + 6;
-            console.log(headerPicWidth); 
+            // console.log(headerPicWidth); 
             
-            console.log(sideMargin);
+            // console.log(sideMargin);
         
             return -(headerPicWidth + sideMargin*2)
     },
